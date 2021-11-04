274f5d250a04409d06aae67caa0ab589d3db6d05
Billi Ramirez <ramirezbilly42@outlook.com>
2021-11-04 15:07:31 -0600

    consolidate data into a single webgl vertex array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var gl,
   shaderProgram,
   vertices,
   matrix = mat4.create(),
-  vertexCount = 30;
+  vertexCount = 36;
 
 initGL();
 createShaders();
@@ -30,33 +30,68 @@ function createShaders() {
 }
 
 function createVertices() {
-  vertices = [];
-  var colors = [];
-  for (var i = 0; i < vertexCount; i++) {
-    vertices.push(Math.random() * 2 - 1);
-    vertices.push(Math.random() * 2 - 1);
-    vertices.push(Math.random() * 2 - 1);
-    colors.push(Math.random());
-    colors.push(Math.random());
-    colors.push(Math.random());
-    colors.push(1);
-  }
+  vertices = [
+    0.88, -0.25, -0.18, 1, 0, 0, 1, 0.9, 0.25, 0, 1, 0, 0, 1, 0.88, -0.25, 0.18,
+    1, 0, 0, 1,
+
+    0.85, -0.25, 0.29, 1, 1, 0, 1, 0.78, 0.25, 0.45, 1, 1, 0, 1, 0.67, -0.25,
+    0.6, 1, 1, 0, 1,
+
+    0.6, -0.25, 0.67, 0, 1, 0, 1, 0.45, 0.25, 0.78, 0, 1, 0, 1, 0.29, -0.25,
+    0.85, 0, 1, 0, 1,
+
+    0.18, -0.25, 0.88, 0, 1, 1, 1, 0, 0.25, 0.9, 0, 1, 1, 1, -0.18, -0.25, 0.88,
+    0, 1, 1, 1,
+
+    -0.29, -0.25, 0.85, 0, 0, 1, 1, -0.45, 0.25, 0.78, 1, 1, 0, 1, -0.6, -0.25,
+    0.67, 0, 0, 1, 1,
+
+    -0.67, -0.25, 0.6, 1, 0, 1, 1, -0.78, 0.25, 0.45, 1, 0, 1, 1, -0.85, -0.25,
+    0.29, 1, 0, 1, 1,
+
+    -0.88, -0.25, 0.18, 1, 0.5, 0, 1, -0.9, 0.25, 0, 1, 0.5, 0, 1, -0.88, -0.25,
+    -0.18, 1, 0.5, 0, 1,
+
+    -0.85, -0.25, -0.29, 0, 0.5, 1, 1, -0.78, 0.25, -0.45, 0, 0.5, 1, 1, -0.67,
+    -0.25, -0.6, 0, 0.5, 1, 1,
+
+    -0.6, -0.25, -0.67, 0, 1, 0.5, 1, -0.45, 0.25, -0.78, 0, 1, 0.5, 1, -0.29,
+    -0.25, -0.85, 0, 1, 0.5, 1,
+
+    -0.18, -0.25, -0.88, 1, 0, 0.5, 1, 0, 0.25, -0.9, 1, 0, 0.5, 1, 0.18, -0.25,
+    -0.88, 1, 0, 0.5, 1,
+
+    0.29, -0.25, -0.85, 0.5, 1, 0, 1, 0.45, 0.25, -0.78, 0.5, 1, 0, 1, 0.6,
+    -0.25, -0.67, 0.5, 1, 0, 1,
+
+    0.67, -0.25, -0.6, 0.5, 0, 1, 1, 0.78, 0.25, -0.45, 0.5, 0, 1, 1, 0.85,
+    -0.25, -0.29, 0.5, 0, 1, 1,
+  ];
 
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   var coords = gl.getAttribLocation(shaderProgram, "coords");
-  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(
+    coords,
+    3,
+    gl.FLOAT,
+    false,
+    Float32Array.BYTES_PER_ELEMENT * 7,
+    0
+  );
   gl.enableVertexAttribArray(coords);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  var colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
   var colorsLocation = gl.getAttribLocation(shaderProgram, "colors");
-  gl.vertexAttribPointer(colorsLocation, 4, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(
+    colorsLocation,
+    4,
+    gl.FLOAT,
+    false,
+    Float32Array.BYTES_PER_ELEMENT * 7,
+    Float32Array.BYTES_PER_ELEMENT * 3
+  );
   gl.enableVertexAttribArray(colorsLocation);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
